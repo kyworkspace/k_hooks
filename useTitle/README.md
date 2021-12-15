12e7d3053e05b60ff6dc86f5a7e5184b610a0d2b
Park Ki Young <ky91zzang@naver.com>
2021-12-15 17:42:04 +0900

    after npm upload

---
+++ useTitle/README.md
@@ -0,0 +1,3 @@
+# @pky_hooks/use-title
+
+React Hook to update your document's totle
--- useTitle/useTitle.js
+++ useTitle/useTitle.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
-export const useTitle = (initialTitle)=>{
-    const [title, setTitle] = useState(initialTitle);
-  
-    const updateTitle = () =>{ // page title element text change
-      const htmlTitle = document.querySelector('title');
-      htmlTitle.innerText=title;
-    }
-  
-    // affected by title changed
-    useEffect(() => {
-      updateTitle()
-    }, [title])
-  
-    return setTitle;
+export const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+
+  const updateTitle = () => { // page title element text change
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
   }
+
+  // affected by title changed
+  useEffect(() => {
+    updateTitle()
+  }, [title])
+
+  return setTitle;
+}
